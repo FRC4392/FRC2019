d35ca992fa1707ef3cb9aa24c08adf591c00152f
DeceiverHP <FRC4392@gmail.com>
2019-01-22 16:11:21 -0500

    Added talons and victor, maybe.

--- src/main/java/frc/robot/Subsystems/Fourbar.java
+++ src/main/java/frc/robot/Subsystems/Fourbar.java
@@ -4,5 +4,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Fourbar{
+    private TalonSRX lift, intake, climber;
+    private VictorSPX intake;
 
 }
