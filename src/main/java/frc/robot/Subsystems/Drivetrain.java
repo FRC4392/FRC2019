f3d3b84d4cbd2fc4dfb46000a5cf4efabd72d9aa
DeceiverHP <FRC4392@gmail.com>
2019-01-22 16:36:50 -0500

    I tried.

--- src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -4,6 +4,22 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Drivetrain{
-    private TalonSRX left, right;
+    private TalonSRX Left, Right;
     private VictorSPX left1,left2, right1, right2;
+    public void setLeftRight(double left, double right) {
+        Left.set(ControlMode.PercentOutput, -left);
+        left1.set(ControlMode.PercentOutput, -left);
+        left2.set(ControlMode.PercentOutput, -left);
+        
+        Right.set(ControlMode.PercentOutput, -right)
+        right1.set(ControlMode.PercentOutput, -right);
+        right2.set(ControlMode.PercentOutput, -right);
+
+        SmartDashboard.putNumber("Left", left);
+        SmartDashboard.putNumber("Right", right);
+    }
+        
+    }() {
+        
+    }
 }
