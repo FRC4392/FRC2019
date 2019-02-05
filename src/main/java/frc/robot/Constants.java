935ec1dddd3f9df105f23dffa52006f92d2918a9
Kristopher Zakfeld <kzakfeld@Kristophers-MacBook-Pro.local>
2019-02-04 20:00:24 -0500

    Started Adding constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+public class Constants {
+
+    //Drivetrain Constants
+    //Drivetrain Motor Controller IDs
+    public static final Integer DrivetrainLeft1DeviceID = 10;
+    public static final Integer DrivetrainLeft2DeviceID = 10;
+    public static final Integer DrivetrainLeft3DeviceID = 10;
+    public static final Integer DrivetrainRight1DeviceID = 10;
+    public static final Integer DrivetrainRight2DeviceID = 10;
+    public static final Integer DrivetrainRight3DeviceID = 10;
+
+    //Drivetrain Parameters
+    public static final Double DrivetrainNominalOutputForward = 1.0;
+    public static final Double DrivetrainNominalOutputReverse = -1.0;
+    public static final Double DrivetrainPeakOutputForward = 1.0;
+    public static final Double DrivetrainPeakOutputReverse = -1.0;
+
+    //Left parameters
+    public static final Boolean LeftDrivetrainSensorPhase = false;
+    public static final Boolean LeftDriveMotorsInverted = false;
+
+    //Right Parameters
+}
--- src/main/java/frc/robot/Subsystems/Drivetrain.java
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -5,78 +5,72 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants;
 
 public class Drivetrain{
-    private TalonSRX Left, Right;
-    private VictorSPX left1,left2, right1, right2;
+    private TalonSRX mLeft1, mRight1;
+    private VictorSPX mLeft2, mLeft3, mRight2, mRight3;
+
+    public Drivetrain() {
+        mLeft1 = new TalonSRX(Constants.DrivetrainLeft1DeviceID);
+        mRight1 = new TalonSRX(Constants.DrivetrainRight1DeviceID);
+        mLeft2 = new VictorSPX(Constants.DrivetrainLeft2DeviceID);
+        mLeft3 = new VictorSPX(Constants.DrivetrainLeft3DeviceID);
+        mRight2 = new VictorSPX(Constants.DrivetrainRight2DeviceID);
+        mRight3 = new VictorSPX(Constants.DrivetrainRight3DeviceID);
+
+        mLeft1.configSelectedFeedbackSensor(feedbackDevice);
+        mLeft1.setSensorPhase(Constants.LeftDrivetrainSensorPhase);
+        mLeft1.setInverted(Constants.LeftDriveMotorsInverted);
+        mLeft1.setStatusFramePeriod(frame, periodMs);
+        mLeft1.configNominalOutputForward(Constants.DrivetrainNominalOutputForward);
+        mLeft1.configNominalOutputReverse(Constants.DrivetrainNominalOutputReverse);
+        mLeft1.configPeakOutputForward(Constants.DrivetrainPeakOutputForward);
+        mLeft1.configPeakOutputReverse(Constants.DrivetrainPeakOutputReverse);
+        mLeft1.selectProfileSlot(slotIdx, pidIdx);
+        mLeft1.config_kF(slotIdx, value, timeoutMs);
+        mLeft1.config_kP(slotIdx, value, timeoutMs);
+        mLeft1.configMotionCruiseVelocity(sensorUnitsPer100ms);
+        mLeft1.configMotionAcceleration(sensorUnitsPer100msPerSec);
+        mLeft1.setNeutralMode(neutralMode);
+
+        mLeft2.follow(mLeft1);
+        mLeft2.setInverted(true);
+        mLeft2.setNeutralMode(neutralMode);
+        mLeft3.follow(mLeft1);
+        mLeft3.setInverted(true);
+        mLeft3.setNeutralMode(neutralMode);
+
+        mRight1.configSelectedFeedbackSensor(feedbackDevice);
+        mRight1.setSensorPhase(false);
+        mRight1.setInverted(true);
+        mRight1.setStatusFramePeriod(frame, periodMs);
+        mRight1.setStatusFramePeriod(frame, periodMs);
+        mRight1.configNominalOutputForward(percentOut);
+        mRight1.configNominalOutputReverse(percentOut);
+        mRight1.configPeakOutputForward(percentOut);
+        mRight1.configPeakOutputReverse(percentOut);
+        mRight1.selectProfileSlot(slotIdx, pidIdx);
+        mRight1.config_kF(slotIdx, value, timeoutMs);
+        mRight1.config_kP(slotIdx, value, timeoutMs);
+        mRight1.configMotionCruiseVelocity(sensorUnitsPer100ms);
+        mRight1.configMotionAcceleration(sensorUnitsPer100msPerSec);
+        mRight1.setNeutralMode(neutralMode);
+
+        mRight2.follow(mRight1);
+        mRight2.setInverted(false);
+        mRight2.setNeutralMode(neutralMode);
+        mRight3.follow(mRight1);
+        mRight3.setInverted(false);
+        mRight3.setNeutralMode(neutralMode);
+
+    }
+
     public void setLeftRight(double left, double right){
-        Left.set(ControlMode.PercentOutput, -left);
-        left1.set(ControlMode.PercentOutput, -left);
-        left2.set(ControlMode.PercentOutput, -left);
+        mLeft1.set(ControlMode.PercentOutput, -left);
         
-        Right.set(ControlMode.PercentOutput, -right);
-        right1.set(ControlMode.PercentOutput, -right);
-        right2.set(ControlMode.PercentOutput, -right);
-
-        SmartDashboard.putNumber("Left", left);
-        SmartDashboard.putNumber("Right", right);}
-        public Drivetrain() {
-        Left = new TalonSRX(deviceNumber);
-        Right = new TalonSRX(deviceNumber);
-        left1 = new VictorSPX(deviceNumber);
-        left2 = new VictorSPX(deviceNumber);
-        right1 = new VictorSPX(deviceNumber);
-        right2 = new VictorSPX(deviceNumber);
-
-        pidgey = new PidgeonIMU();
-
-        Left.configSelectedFeedbackSensor(feedbackDevice);
-        Left.setSensorPhase(false);
-        Left.setInverted(false);
-        Left.setStatusFramePeriod(frame, periodMs);
-        Left.setStatusFramePeriod(frame, periodMs);
-        Left.configNominalOutputForward(percentOut);
-        Left.configNominalOutputReverse(percentOut);
-        Left.configPeakOutputForward(percentOut);
-        Left.configPeakOutputReverse(percentOut);
-        Left.selectProfileSlot(slotIdx, pidIdx);
-        Left.config_kF(slotIdx, value, timeoutMs);
-        Left.config_kP(slotIdx, value, timeoutMs);
-        Left.configMotionCruiseVelocity(sensorUnitsPer100ms);
-        Left.configMotionAcceleration(sensorUnitsPer100msPerSec);
-        Left.setNeutralMode(neutralMode);
-
-        left1.follow(Left);
-        left1.setInverted(true);
-        left1.setNeutralMode(neutralMode);
-        left2.follow(Left);
-        left2.setInverted(true);
-        left2.setNeutralMode(neutralMode);
-
-        Right.configSelectedFeedbackSensor(feedbackDevice);
-        Right.setSensorPhase(false);
-        Right.setInverted(true);
-        Right.setStatusFramePeriod(frame, periodMs);
-        Right.setStatusFramePeriod(frame, periodMs);
-        Right.configNominalOutputForward(percentOut);
-        Right.configNominalOutputReverse(percentOut);
-        Right.configPeakOutputForward(percentOut);
-        Right.configPeakOutputReverse(percentOut);
-        Right.selectProfileSlot(slotIdx, pidIdx);
-        Right.config_kF(slotIdx, value, timeoutMs);
-        Right.config_kP(slotIdx, value, timeoutMs);
-        Right.configMotionCruiseVelocity(sensorUnitsPer100ms);
-        Right.configMotionAcceleration(sensorUnitsPer100msPerSec);
-        Right.setNeutralMode(neutralMode);
-
-        right1.follow(right1);
-        right1.setInverted(false);
-        right1.setNeutralMode(neutralMode);
-        right2.follow(right2);
-        right2.setInverted(false);
-        right2.setNeutralMode(neutralMode);
-
-        }
-    } 
+        mRight1.set(ControlMode.PercentOutput, -right);
+    }
+} 
         
 
