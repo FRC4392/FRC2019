6f26ed71a14702c9debfaf8ebec27cddfa5f0e13
DeceiverHP <FRC4392@gmail.com>
2019-02-05 19:13:46 -0500

    Lift Constructor

--- src/main/java/frc/robot/Subsystems/Lift.java
+++ src/main/java/frc/robot/Subsystems/Lift.java
@@ -1,12 +1,31 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Lift{
-    private TalonSRX Motor1;
-    private TalonSRX Motor2;
-    public void init() {
-        
+    private TalonSRX mMotor1;
+    private TalonSRX mMotor2;
+    public Lift() {
+        mMotor1.configSelectedFeedbackSensor(feedbackDevice);
+        mMotor1.setSensorPhase(false);
+        mMotor1.setInverted(false);
+        mMotor1.setStatusFramePeriod(frame, periodMs);
+        mMotor1.setStatusFramePeriod(frame, periodMs);
+        mMotor1.configNominalOutputForward(percentOut);
+        mMotor1.configNominalOutputReverse(percentOut);
+        mMotor1.configPeakOutputForward(percentOut);
+        mMotor1.configPeakOutputReverse(percentOut);
+        mMotor1.selectProfileSlot(slotIdx, pidIdx);
+        mMotor1.config_kF(slotIdx, value);
+        mMotor1.config_kP(slotIdx, value);
+        mMotor1.configMotionCruiseVelocity(sensorUnitsPer100ms);
+        mMotor1.configMotionAcceleration(sensorUnitsPer100msPerSec);
+        mMotor1.setNeutralMode(NeutralMode.Brake);
+
+        mMotor2.setInverted(true);
+        mMotor2.follow(mMotor1);
+        mMotor2.setNeutralMode(NeutralMode.Brake);
     }
 }
