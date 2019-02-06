c2466eed7901cd8154cf5c5afb77c6ed4b529072
DeceiverHP <FRC4392@gmail.com>
2019-02-06 17:32:49 -0500

    Intake

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -1,7 +1,53 @@
 package frc.robot.Subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Solenoid;
+
 public class Intake{
     
-public void init() {
+    double intakeSpeed = -1;
+    double outtakeSpeed = .5;
+
+    private TalonSRX mintake1 = new TalonSRX(deviceNumber);
+    private TalonSRX mintake2 = new TalonSRX(deviceNumber);
+    private Solenoid mjaw = new Solenoid(channel);
+    private Solenoid mPivot = new Solenoid(channel);
+
+    public Intake(){
+        mintake1.follow(mintake2);
+        mintake1.setInverted(true);
+        mintake1.setNeutralMode(NeutralMode.Brake);
+        mintake2.setNeutralMode(NeutralMode.Brake);
+    }
     
-}
+    public void setSpeed(double speed) {
+        mintake2.set(ControlMode.PercentOutput, speed);
+    }
+
+    public void setIntake() {
+        setSpeed(intakeSpeed);
+    }
+
+    public void setOuttake() {
+        setSpeed(outtakeSpeed);    
+    }
+
+    public void stop() {
+        setSpeed(0);
+    }
+
+    public void setPivotUp() {
+        setPivot(true);
+    }
+
+    public void setPivotDown() {
+        setPivot(false);
+    }
+
+    public void setPivot(boolean down) {
+        Pivot.set(down);        
+    }
 }
