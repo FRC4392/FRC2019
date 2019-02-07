e68e2527076ae714e4cd4bd5ae85ecec3690146c
DeceiverHP <FRC4392@gmail.com>
2019-02-07 13:06:35 -0500

    Fourbar Constructor

--- src/main/java/frc/robot/Subsystems/Fourbar.java
+++ src/main/java/frc/robot/Subsystems/Fourbar.java
@@ -4,10 +4,28 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Fourbar{
-    private TalonSRX IntakePivot, ArmPivot;
+    private TalonSRX mIntakePivot, mArmPivot;
 
     public Fourbar(){
+        mIntakePivot.configSelectedFeedbackSensor(feedbackDevice);
+        mIntakePivot.setSensorPhase(false);
+        mIntakePivot.setInverted(false);
+        mIntakePivot.setStatusFramePeriod(frame, periodMs);
+        mIntakePivot.setStatusFramePeriod(frame, periodMs);
+        mIntakePivot.configNominalOutputForward(percentOut);
+        mIntakePivot.configNominalOutputReverse(percentOut);
+        mIntakePivot.configPeakOutputForward(percentOut);
+        mIntakePivot.configPeakOutputReverse(percentOut);
+        mIntakePivot.selectProfileSlot(slotIdx, pidIdx);
+        mIntakePivot.config_kF(slotIdx, value);
+        mIntakePivot.config_kP(slotIdx, value);
+        mIntakePivot.configMotionCruiseVelocity(sensorUnitsPer100ms);
+        mIntakePivot.configMotionAcceleration(sensorUnitsPer100msPerSec);
+        mIntakePivot.setNeutralMode(neutralMode);
 
+        mArmPivot.setInverted(true);
+        mArmPivot.follow(mIntakePivot);
+        mArmPivot.setNeutralMode(neutralMode);
     }
 
 }
